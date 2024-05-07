60d2de40f02adeb068b8ca44cfc94bde1ea422b5
Aditya Ranjan <136574821+Aditya-Ranjan1234@users.noreply.github.com>
2024-05-07 22:05:26 +0530

    Create 5.LongestPalindromicString.java

---
+++ String/Medium/5.LongestPalindromicString.java
@@ -0,0 +1,133 @@
+//Manacher's Algorithm - O(n) best 
+
+class Solution {
+    public String longestPalindrome(String s) {
+        StringBuilder sPrime = new StringBuilder("#");
+        for (char c : s.toCharArray()) {
+            sPrime.append(c).append("#");
+        }
+
+        int n = sPrime.length();
+        int[] palindromeRadii = new int[n];
+        int center = 0;
+        int radius = 0;
+
+        for (int i = 0; i < n; i++) {
+            int mirror = 2 * center - i;
+
+            if (i < radius) {
+                palindromeRadii[i] = Math.min(
+                    radius - i,
+                    palindromeRadii[mirror]
+                );
+            }
+
+            while (
+                i + 1 + palindromeRadii[i] < n &&
+                i - 1 - palindromeRadii[i] >= 0 &&
+                sPrime.charAt(i + 1 + palindromeRadii[i]) ==
+                    sPrime.charAt(i - 1 - palindromeRadii[i])
+            ) {
+                palindromeRadii[i]++;
+            }
+
+            if (i + palindromeRadii[i] > radius) {
+                center = i;
+                radius = i + palindromeRadii[i];
+            }
+        }
+
+        int maxLength = 0;
+        int centerIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (palindromeRadii[i] > maxLength) {
+                maxLength = palindromeRadii[i];
+                centerIndex = i;
+            }
+        }
+
+        int startIndex = (centerIndex - maxLength) / 2;
+        String longestPalindrome = s.substring(
+            startIndex,
+            startIndex + maxLength
+        );
+
+        return longestPalindrome;
+    }
+}
+
+
+
+/*
+
+1. Brute Force
+
+class Solution {
+    public String longestPalindrome(String s) {
+        for (int length = s.length(); length > 0; length--) {
+            for (int start = 0; start <= s.length() - length; start++) {
+                if (check(start, start + length, s)) {
+                    return s.substring(start, start + length);
+                }
+            }
+        }
+
+        return "";
+    }
+
+    private boolean check(int i, int j, String s) {
+        int left = i;
+        int right = j - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
+2. Dynamic Programming (need to learn)
+
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int[] ans = new int[] { 0, 0 };
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                ans[0] = i;
+                ans[1] = i + 1;
+            }
+        }
+
+        for (int diff = 2; diff < n; diff++) {
+            for (int i = 0; i < n - diff; i++) {
+                int j = i + diff;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+
+        int i = ans[0];
+        int j = ans[1];
+        return s.substring(i, j + 1);
+    }
+}
+
+
+*/
